f628f97ed6ced42668a3d53beecf8711c569cc2f
Amaan Jambura <amaanjambura786@gmail.com>
2023-03-04 21:25:50 +0530

    Added custom modal in the page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -2,7 +2,11 @@ import React,{useState} from 'react'
 import './navbar.css'
 import {SiYourtraveldottv} from 'react-icons/si'
 import {AiFillCloseCircle} from 'react-icons/ai'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
 import {TbGridDots} from 'react-icons/tb'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Navbar = () => {
   const[active, setActive] = useState('navBar')
@@ -15,6 +19,11 @@ const Navbar = () => {
   const removeNavbar = () =>{
     setActive('navBar ')
   }
+
+  const [show, setShow] = useState(false);
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
 
 
   return (
@@ -45,12 +54,11 @@ const Navbar = () => {
               <a href="#" className="navLink">News</a>
             </li>
 
-          <button className='btn'>
+          <button className='btn' onClick={handleShow}>
             <a href='#'>BOOK NOW</a>
           </button>
 
           </ul>
-
           <div onClick={removeNavbar} className="closeNavbar">
             <AiFillCloseCircle className='icon'/>
           </div>
@@ -59,7 +67,38 @@ const Navbar = () => {
         <div onClick={showNav} className="toggleNavbar">
           <TbGridDots className='icon'/>
         </div>
-
+        <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Modal heading</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="name@example.com"
+                autoFocus
+              />
+            </Form.Group>
+            <Form.Group
+              className="mb-3"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Label>Example textarea</Form.Label>
+              <Form.Control as="textarea" rows={3} />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleClose}>
+            Close
+          </Button>
+          <Button variant="primary" onClick={handleClose}>
+            Save Changes
+          </Button>
+        </Modal.Footer>
+      </Modal>
       </header>
     </section>
   )
